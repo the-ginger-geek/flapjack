207a72dff7e50a2b459e29b8f7cb7bcfbe85b710
neilhogg <6nDpEsWPqJegskZF7sG_>
2023-07-25 21:08:24 +0200

    Moved builders file to the generators package

--- lib/src/generators/builders.dart
+++ lib/src/generators/builders.dart
@@ -1,6 +1,6 @@
 import 'package:source_gen/source_gen.dart';
 import 'package:build/build.dart';
-import 'src/generators/generators.dart';
+import 'generators.dart';
 
 Builder loadingMethodBuilder(BuilderOptions options) =>
     PartBuilder([LoadingMethodGenerator()], '.flapjack.dart');

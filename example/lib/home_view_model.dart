b4fc7d82a5eb1b4401987584b1fea7b9cb8087ae
neilhogg <6nDpEsWPqJegskZF7sG_>
2023-08-25 19:24:42 +0200

    Got rid of useless generated methods

--- example/lib/home_view_model.dart
+++ example/lib/home_view_model.dart
@@ -2,20 +2,17 @@ import 'package:flapjack/flapjack.dart';
 
 import 'home_model.dart';
 
-part 'home_view_model.flapjack.dart';
-
 class HomeViewModel extends FlapjackViewModel<HomeModel> {
   HomeViewModel(super.model);
 
   int get counter => model.counter;
 
-  @AsyncLoader(loaderKey: 'increment')
-  Future _$increment(int amount) async {
+  Future increment(int amount) async {
     await Future.delayed(const Duration(milliseconds: 360));
     model.counter+=amount;
   }
-  @AsyncLoader(loaderKey: 'decrement')
-  Future _$decrement(int amount) async {
+
+  Future decrement(int amount) async {
     await Future.delayed(const Duration(milliseconds: 360));
     model.counter-=amount;
   }
